3108a0ce64413b715a0e83d210a4f92ed92f3ba3
yattyo <yattyo@pixiv.com>
2017-05-01 12:46:19 +0900

    文章投稿機能・タイムライン表示を追加した

--- post_toot.php
+++ post_toot.php
@@ -5,4 +5,20 @@ require_once 'functions.php';
 session_start();
 redirectToLoginPageIfNotLoggedIn();
 
+$text = $_POST['text'];
+$timestamp = date("Y-m-d H:i:s");
+
+$database = getDatabase();
+$database->query("
+    INSERT INTO `toot` (
+        `user_id`,
+        `text`,
+        `created_at`
+        ) VALUES (
+          '{$_SESSION['user_id']}',
+          '{$text}',
+          '{$timestamp}'
+          )
+    ");
+
 header('Location: /');
