169615f79adf3d12b40af21b74322274bbfd42fe
yattyo <yattyo@pixiv.com>
2017-05-02 14:03:41 +0900

    リプライとファボをつくった

--- index.php
+++ index.php
@@ -23,7 +23,15 @@ $toots = $database->query("
     <head>
         <title>Masatodon(マサトドン)</title>
         <link rel="stylesheet" href="/css/style.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/modaal/0.3.1/css/modaal.min.css">
+        <script
+          src="https://code.jquery.com/jquery-3.2.1.min.js"
+          integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
+          crossorigin="anonymous">
+        </script>
+        <script src="https://cdn.jsdelivr.net/modaal/0.3.1/js/modaal.min.js"></script>
         <meta charset="utf-8">
+
     </head>
     <body>
         <div class="wrapper">
@@ -36,11 +44,11 @@ $toots = $database->query("
                     <div class="user_id">@<?php echo $_SESSION['user_login_name']; ?></div>
                 </div>
                 <form enctype="multipart/form-data" method="post" action="/post_toot.php">
-                    <textarea name="text" placeholder="今なにしてる？" required></textarea>
+                    <textarea class="textbox" name="text" placeholder="今なにしてる？" required></textarea>
                     <input type="file" name="image">
                     <div class="toot-button-container">
                         <input type="submit" class="toot-button" value="トゥート!">
-                        <a href="/change_icon.php">アイコン変えるよ</a>
+
                     </div>
                 </form>
             </div>
@@ -52,15 +60,21 @@ $toots = $database->query("
                 <?php foreach($toots as $toot){ ?>
                     <li>
                       <img class="user-icon" src= "<?php echo displayIcon($toot['icon_image']) ?>" width="30">
-                      <div class="iconyoko">
-                        <div class="yasunoyoko">
-                          <div><?php echo $toot["display_name"]; ?></div>
-                          <div class="user_id">@<?php echo $toot["login_name"]; ?></div>
-                        </div>
-                          <p><?php echo $toot["text"]; ?></p>
-                            <?php if($toot["image_file_name"] != null ) { ?>
+                      <div class="toot-content">
+                          <span><?php echo $toot["display_name"]; ?></span>
+                          <span class="user_id">@<?php echo $toot["login_name"]; ?></span>
+                          <p><?php echo str_replace ( "@" . $_SESSION['user_login_name'] . " " , '<span class="called_name">@' . $_SESSION['user_login_name'] . '</span> '  , $toot["text"] ); ?></p>
+
+                          <?php if($toot["image_file_name"] != null ) { ?>
+                            <a href="/uploaded_image/<?php echo $toot['image_file_name'];?>" class="toot-modal">
                               <img class="toot-image" src= "/uploaded_image/<?php echo $toot['image_file_name'];?>">
-                            <?php } ?>
+                            </a>
+                          <?php } ?>
+                          <div class="buttons">
+                            <img class="reply_button" src="/img/reply_button.png">
+                            <img class="fav_button" src="/img/fav_button.png">
+                            <span class="fav_num">0</span>
+                          </div>
                       </div>
                     </li>
                 <?php } ?>
@@ -79,5 +93,30 @@ $toots = $database->query("
                 </div>
             </div>
         </div>
+        <script>
+          $(function(){
+            $('.toot-modal').modaal({
+              type: 'image'
+            });
+
+            $('.buttons .reply_button').on(
+              'click',
+              function(){
+                var login_name = $(this).closest('.toot-content').find('.user_id').text();
+                $('textarea.textbox').val(login_name + " ");
+                $('textarea.textbox').focus();
+              }
+            )
+
+            $('.buttons .fav_button').on(
+              'click',
+              function(){
+                $(this).attr("src","/img/fav_button_yellow.png");
+                var now_fav_num = $(this).next('.fav_num').text();
+                $(this).next('.fav_num').text(Number(now_fav_num) + 1);
+              })
+          })
+        </script>
+
     </body>
 </html>
